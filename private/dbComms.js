fcb7a27a97705fe38ec8c7aa3c8e85087775f537
rsmarsh <missingmarshall@gmail.com>
2017-07-05 18:20:03 +0100

    added new tracked files

---
+++ private/dbComms.js
@@ -0,0 +1,17 @@
+var nodeDB = require('nodejs sql something');
+
+var dbUsername = "dbuser";
+var dbPassword = "dbpassword";
+
+var insertIntoDB = function(data) {
+	var query = "INSERT INTO x DATA y";
+};
+
+var selectFromDB = function(data) {
+	var query = xxxxx;	
+};
+
+
+module.exports = {
+ insertIntoDB: insertIntoDB,
+}
